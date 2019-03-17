ba438343def0244192fb016946591a55742ce29a
eaglekindoms <huyongmao35@outlook.com>
2019-03-17 10:18:04 +0800

    Update htmldownload.rs

--- download/htmldownload.rs
+++ download/htmldownload.rs
@@ -6,25 +6,42 @@ use std::io::Write;
 
 /// Remember url and file_path are both string slice which don't need to reference again.
 /// so when use them, we can ignore the "&".
-/// this is a function to download html's text from http path
-pub fn get_html(url: &str, file_path: &str) {
+/// when match the Err(_) should contain "panic" marco, or return same type,
+/// otherwise the variable will have two possibilities.
 
-    //create file which doesn't exist.
-    if  !Path::exists(Path::new(file_path)) {
+// request html text.
+pub fn get_html(url: &str) -> String {
 
-        File::create(file_path).unwrap();
+    // get the text from url's path and deal with error.
+    let html_text = match reqwest::get(url).unwrap().text() {
 
-    }
-
-    //open file as append
-    let mut file = OpenOptions::new().append(true).open(file_path).unwrap();
-
-    //get the text from url's path and deal with error
-    let _html_error_result = match reqwest::get(url).unwrap().text() {
-
-        Ok(html_text) => file.write(html_text.as_bytes()),
-        Err(_)  => panic!("sorry, unknown error!"),
+        Ok(html_text) => html_text.to_string(),
+        Err(_)  => panic!("sorry, can't get html page!"),
 
     };
+    return html_text;
+}
+// store text into file.
+pub fn write_html(text: &str, file_path: &str, model: char) {
+
+    // create file which doesn't exist.
+    if  !Path::exists(Path::new(file_path)) {
+        File::create(file_path).unwrap();
+    }
+
+    match model {
+        'a' => {
+            // append text into file, but don't truncate it.
+            let mut file = OpenOptions::new().append(true).open(file_path).unwrap();
+            file.write(text.as_bytes()).unwrap();
+        },
+        'w' => {
+            // truncate file, and write text into file.
+            let mut file = OpenOptions::new().write(true)
+                                        .truncate(true).open(file_path).unwrap();
+            file.write(text.as_bytes()).unwrap();
+        },
+         _  => panic!("invalid char"),
+    }
 
 }
